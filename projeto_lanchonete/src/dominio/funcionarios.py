4a2078e85fc564d6777022b370ef720c630b6aad
oliviabez <olivia.bezerra@escolar.ifrn.edu.br>
2026-07-14 19:07:13 -0300

    Delete projeto_lanchonete directory

--- projeto_lanchonete/src/dominio/funcionarios.py
+++
@@ -1,7 +0,0 @@
-
-class Funcionarios:
-    def __init__(self, id_funcionarios: int | None, nome: str, usuario: str, senha: str):
-        self.id_funcionarios = id_funcionarios
-        self.nome = nome
-        self.usuario = usuario
-        self.senha = senha
--- projeto_lanchonete/src/dominio/item_pedido.py
+++
@@ -1,7 +0,0 @@
-class ItensPedido:
-    def __init__(self, id_item: int | None, id_pedido: int, id_produto: int, quantidade: int, preco_unitario: float):
-        self.id_item = id_item
-        self.id_pedido = id_pedido
-        self.id_produto = id_produto
-        self.quantidade = quantidade
-        self.preco_unitario = preco_unitario
